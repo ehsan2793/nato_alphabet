2aeee9379fa72bef98d1e084b7ca2f6d7ab1db18
ehsan2793 <ehsan2793@gmail.com>
2022-02-27 08:41:36 -0800

    all done

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import pandas as p
-data  = p.read_csv('nato-alphabet.csv')
+
+data = p.read_csv('nato-alphabet.csv')
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+word = input("Enter a word: ").upper()
+
+nato = [phonetic_dict[item] for item in word ]
+
+print(nato)
